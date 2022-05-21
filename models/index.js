4ac18b65dd02fd29b385c573a9b7c750e942044a
Owen Taylor <Owentaylor60@yahoo.com>
2022-05-21 15:41:19 -0400

    Vote added

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const User = require('./Users');
 const Post = require('./Post')
-
+const Vote = require('./Vote')
 // create associations
 User.hasMany(Post, {
     foreignKey: 'user_id'
@@ -9,5 +9,32 @@ User.hasMany(Post, {
   Post.belongsTo(User, {
     foreignKey: 'user_id',
   });
+
+User.belongsToMany(Post, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'user_id'
+});
+
+Post.belongsToMany(User, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'post_id'
+});
+
+Vote.belongsTo(User, {
+    foreignKey: 'user_id'
+  });
   
-module.exports = { User, Post };
+  Vote.belongsTo(Post, {
+    foreignKey: 'post_id'
+  });
+  
+  User.hasMany(Vote, {
+    foreignKey: 'user_id'
+  });
+  
+  Post.hasMany(Vote, {
+    foreignKey: 'post_id'
+  });
+module.exports = { User, Post, Vote };
